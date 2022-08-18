d8ac0cb69ce5175ef3de86a1f141c8dcaf6a1f79
unknown <vikogutu77@gmail.com>
2022-08-18 12:45:19 +0300

    urls selection

--- app/src/main/java/com/mhealthkenya/psurvey/depedancies/Constants.java
+++ app/src/main/java/com/mhealthkenya/psurvey/depedancies/Constants.java
@@ -8,36 +8,34 @@ public class Constants {
     /*ENDPOINT*/
     //public static String ENDPOINT = "https://psurveyapi.kenyahmis.org/";
     //public static String ENDPOINT="https://prod.kenyahmis.org:9100/";
-    public static String ENDPOINT = "https://psurvey-api.mhealthkenya.co.ke/";
-
-
-
-
+    //Test -https://prod.kenyahmis.org:9000
+    //Prod -https://psurvey-api.mhealthkenya.co.ke
+    public static String ENDPOINT = "https://psurvey-api.mhealthkenya.co.ke";
 
 
     //AUTH
-    public static String SIGNUP = "auth/users/";
-    public static String CURRENT_USER = "auth/users/me";
-    public static String CURRENT_USER_DETAILED = "api/current/user";
-    public static String UPDATE_USER = "auth/users/me/";
-    public static String LOGIN = "auth/token/login";
-    public static String LOGOUT = "auth/token/logout/";
+    public static String SIGNUP = "/auth/users/";
+    public static String CURRENT_USER = "/auth/users/me";
+    public static String CURRENT_USER_DETAILED = "/api/current/user";
+    public static String UPDATE_USER = "/auth/users/me/";
+    public static String LOGIN = "/auth/token/login";
+    public static String LOGOUT = "/auth/token/logout/";
 
 
 
     //FACILITIES AND DESIGNATIONS
-    public static String ALL_FACILITIES = "api/facilities";
-    public static String DESIGNATION = "api/designation";
+    public static String ALL_FACILITIES = "/api/facilities";
+    public static String DESIGNATION = "/api/designation";
 
 
 
     //QUESTIONNAIRES
-    public static String ACTIVE_SURVEYS = "api/questionnaire/active";
-    public static String ALL_QUESTIONNAIRES = "api/questionnaire/all";
-    public static String PATIENT_CONSENT = "api/questionnaire/start/";
-    public static String PROVIDE_ANSWER = "api/questions/answer/";
-    public static String INITIAL_CONFIRMATION = "api/initial/consent/";
-    public static String GET_PARTICIPANTS ="api/questionnaire/participants/";
+    public static String ACTIVE_SURVEYS = "/api/questionnaire/active";
+    public static String ALL_QUESTIONNAIRES = "/api/questionnaire/all";
+    public static String PATIENT_CONSENT = "/api/questionnaire/start/";
+    public static String PROVIDE_ANSWER = "/api/questions/answer/";
+    public static String INITIAL_CONFIRMATION = "/api/initial/consent/";
+    public static String GET_PARTICIPANTS ="/api/questionnaire/participants/";
 
 
     /*MODELS*/
